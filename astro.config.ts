bd1a05b0756c2afa0486cd9bc158c245ceeb1bd9
Jianqiang Du <jackdu2013@163.com>
2024-07-28 19:09:05 +0800

    bugfix: base is missing a starting slash

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   site: "https://yurikong.github.io",
-  base: "astro-theme-resume",
+  base: "/astro-theme-resume",
   server: {
     host: true,
     open: true,
