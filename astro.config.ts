2dd7bf834708c67aee9c25541ac01f299c655a6e
Jianqiang Du <jackdu2013@163.com>
2024-08-03 08:00:50 +0800

    disable sitemap & robots for now

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,27 @@
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+// import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yurikong.github.io",
-  base: "/astro-theme-resume",
-  trailingSlash: "always",
+  site: "https://yurikong.github.io", // TODO: change to vercel deploy url
+  base: "/astro-theme-resume", // TODO: change to / when setting for vercel
   server: {
     host: true,
     open: true,
   },
   integrations: [
     tailwind(),
-    sitemap({
-      changefreq: "monthly",
-      priority: 1,
-      lastmod: new Date(),
-      i18n: {
-        defaultLocale: "en",
-        locales: {
-          en: "en-US",
-        },
-      },
-    }),
+    // sitemap({
+    //   changefreq: "monthly",
+    //   priority: 1,
+    //   lastmod: new Date(),
+    //   i18n: {
+    //     defaultLocale: "en",
+    //     locales: {
+    //       en: "en-US",
+    //     },
+    //   },
+    // }),
   ],
 })
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,21 +1,21 @@
-import type { APIRoute } from "astro"
+// import type { APIRoute } from "astro"
 
-const robotsTxt: string = `
-User-agent: *
-Allow: /
+// const robotsTxt: string = `
+// User-agent: *
+// Allow: /
 
-Sitemap: ${
-  new URL(
-    "sitemap-index.xml",
-    new URL(import.meta.env.BASE_URL, import.meta.env.SITE).href
-  ).href
-}
-`.trim()
+// Sitemap: ${
+//   new URL(
+//     "sitemap-index.xml",
+//     new URL(import.meta.env.BASE_URL, import.meta.env.SITE).href
+//   ).href
+// }
+// `.trim()
 
-export const GET: APIRoute = () => {
-  return new Response(robotsTxt, {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  })
-}
+// export const GET: APIRoute = () => {
+//   return new Response(robotsTxt, {
+//     headers: {
+//       "Content-Type": "text/plain; charset=utf-8",
+//     },
+//   })
+// }
