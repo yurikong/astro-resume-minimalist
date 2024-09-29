1e27b4e71e057f413ed662081dfa209e45fa1de5
Jianqiang Du <jackdu2013@163.com>
2024-09-29 16:27:21 +0800

    update mostRecentYearComparator to allow `endYear` to be undefined

--- src/utils/comparator.ts
+++ src/utils/comparator.ts
@@ -5,9 +5,14 @@
  * @param {T} b - Item to compare.
  * @returns {number} Same as the return value of `compareFn` in `Array.sort()`.
  */
-export function mostRecentYearComparator<T extends { startYear: number; endYear: number }>(a: T, b: T): number {
-  if (b.endYear === a.endYear) {
+export function mostRecentYearComparator<T extends { startYear: number; endYear?: number }>(a: T, b: T): number {
+  if (a.endYear === b.endYear) {
     return b.startYear - a.startYear
+  } else if (a.endYear === undefined) {
+    return -1
+  } else if (b.endYear === undefined) {
+    return 1
+  } else {
+    return b.endYear - a.endYear
   }
-  return b.endYear - a.endYear
 }
