db7901b2e127960b23cf5c2ef08236fe9542c101
Jianqiang Du <jackdu2013@163.com>
2024-10-10 09:13:10 +0800

    encapsulate getAllProjects api

---
+++ src/api/project.ts
@@ -0,0 +1,20 @@
+import type { IProject } from "~/types/project"
+import { Repository } from "~/types/repository"
+
+/**
+ * Get all projects.
+ * @returns {Promise<IProject[]>}
+ */
+export async function getAllProjects(): Promise<IProject[]> {
+  const site: string = import.meta.env.DEV ? `http://localhost:${import.meta.env.PORT}` : import.meta.env.SITE
+  const apiUrl: string = `${site}${import.meta.env.BASE_URL}/projects.json`
+  const response: Response = await fetch(apiUrl)
+  const data: IProject[] = await response.json()
+  data.forEach((it) => {
+    if (!!it.repository) {
+      const { owner, name } = it.repository
+      it.repository = new Repository(owner, name)
+    }
+  })
+  return data
+}
