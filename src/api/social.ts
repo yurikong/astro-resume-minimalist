de43f4649e6babacfbfe83c882f7d91807bfb60b
Jianqiang Du <jackdu2013@163.com>
2024-10-14 15:35:12 +0800

    move socials data into endpoint

---
+++ src/api/social.ts
@@ -0,0 +1,13 @@
+import type { ISocial } from "~types/social"
+
+/**
+ * Get all social contacts.
+ * @returns {Promise<ISocial>}
+ */
+export async function getAllSocials(): Promise<ISocial[]> {
+  const site: string = import.meta.env.DEV ? `http://localhost:${import.meta.env.PORT}` : import.meta.env.SITE
+  const apiUrl: string = `${site}${import.meta.env.BASE_URL}/socials.json`
+  const r: Response = await fetch(apiUrl)
+  const data: ISocial[] = await r.json()
+  return data
+}
