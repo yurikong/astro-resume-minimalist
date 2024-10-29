13e71b7e1e0630478c170c7434d55834365dad5d
Jianqiang Du <jackdu2013@163.com>
2024-10-29 10:50:46 +0800

    remove env

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,7 @@
-/// <reference path="./src/env.d.ts" />
-
-import { loadEnv } from "vite"
 import { defineConfig, squooshImageService } from "astro/config"
 import UnoCSS from "unocss/astro"
 import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
-
-const { PORT }: ImportMetaEnv | Record<string, string> = loadEnv(import.meta.env.MODE, process.cwd(), "")
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,8 +12,6 @@ export default defineConfig({
   site: "https://yurikong.github.io",
   base: "astro-theme-resume-minimalist",
   server: {
-    port: Number(PORT),
-    host: true,
     open: true,
   },
   integrations: [
@@ -43,10 +36,7 @@ export default defineConfig({
         output: {
           chunkFileNames: "static/assets/js/[name]-[hash].js",
           entryFileNames: "static/assets/js/[name]-[hash].js",
-          assetFileNames: ({ name }): string => {
-            if (name === undefined) {
-              return "static/assets/[name]-[hash].[ext]"
-            }
+          assetFileNames: ({ name = "" }) => {
             if (/\.css$/i.test(name)) {
               return "static/assets/css/[name]-[hash].css"
             }
