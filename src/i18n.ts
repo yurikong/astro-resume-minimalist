7b44b5357cb61647a8d7c51721d91337893c1d27
Jianqiang Du <jackdu2013@163.com>
2024-10-31 10:39:55 +0800

    redirect to i18n page on index route

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,38 +1,49 @@
 export type TLocale = "en" | "zh"
 
 export const defaultLocale: TLocale = "en"
-// export const supportedLocales: TLocale[] = ["en", "zh"]
+export const supportedLocales: TLocale[] = ["en", "zh"]
 
-// /**
-//  * Whether a locale is supported.
-//  * @param {string} locale
-//  * @returns {boolean}
-//  */
-// export function isLocaleSupported(locale: string): boolean {
-//   return supportedLocales.some((it) => locale.startsWith(it))
-// }
+/**
+ * Whether an input locale string is supportted.
+ * @param {string}locale
+ * @returns {boolean}
+ */
+export function isLocaleSupported(locale: string): boolean {
+  return supportedLocales.some((it) => locale.startsWith(it))
+}
 
-// // function parseLocale(locale: string): TLocale {
-// //   const  = locale.split("-", 1).at(0) ?? defaultLocale
-// // }
+/**
+ * Parse locale from input string, fallbacks to `defaultLocale` if not supported.
+ * @param {string} s
+ * @returns {TLocale}
+ */
+export function parseLocale(s: string): TLocale {
+  const parsedLocaleString: string | undefined = s.split("-", 1).at(0)
+  if (!parsedLocaleString) {
+    return defaultLocale
+  }
+  return isLocaleSupported(parsedLocaleString) ? <TLocale>parsedLocaleString : defaultLocale
+}
 
-// /**
-//  * Get locale from local storage or browser, fallbacks to `en`.
-//  * @returns {string}
-//  */
-// export function getLocale(): string {
-//   const localLocale: string | null = localStorage.getItem("locale")
-//   if (localLocale) {
-//     return isLocaleSupported(localLocale) ? localLocale : defaultLocale
-//   }
-//   const browserLocale: string = navigator.language
-//   return isLocaleSupported(browserLocale) ? browserLocale : defaultLocale
-// }
+/**
+ * Get locale from local storage or browser, fallbacks to `defaultLocale`.
+ * @returns {TLocale}
+ */
+export function getLocale(): TLocale {
+  const locale: string = localStorage.getItem("locale") || navigator.language
+  return parseLocale(locale)
+}
 
-// /**
-//  * Set `locale` to local storage.
-//  * @param {string} locale
-//  */
-// export function setLocale(locale: string): void {
-//   localStorage.setItem("locale", locale)
-// }
+/**
+ * Set `locale` to local storage and redirect to corresponding locale page.
+ * @param {TLocale} locale
+ */
+export function setLocale(locale: TLocale): void {
+  localStorage.setItem("locale", locale)
+  let url: string = window.location.href
+  if (!url.endsWith("/")) {
+    url += "/"
+  }
+  url += locale
+  window.location.href = url
+}
